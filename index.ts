25450d3b0ab8048c1d890593987c4537efbbd7fb
Erik Gaal <me@erikgaal.nl>
2019-10-27 16:58:36 +0100

    Add prettier

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-import core from "@actions/core";
-import github from "@actions/github";
+import core from '@actions/core';
+import github from '@actions/github';
 
 async function run() {
-  const token = core.getInput("token", { required: true });
-  const body = core.getInput("body", { required: true });
+  const token = core.getInput('token', { required: true });
+  const body = core.getInput('body', { required: true });
 
   const octokit = new github.GitHub(token);
   const owner = github.context.issue.owner;
@@ -13,7 +13,7 @@ async function run() {
   const { data: comments } = await octokit.issues.listComments({
     owner,
     repo,
-    issue_number
+    issue_number,
   });
 
   if (!comments.find(c => c.body === body)) {
@@ -21,7 +21,7 @@ async function run() {
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       issue_number: github.context.issue.number,
-      body: body
+      body: body,
     });
   }
 }
